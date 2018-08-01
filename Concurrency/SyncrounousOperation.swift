04ce5f751db1483a5ff97eda7497018bd30ce841
Muhammad Sajad <sajadkhan71@yahoo.com>
2018-08-01 10:28:30 +0500

    check if operation was cancelled

--- Concurrency/SyncrounousOperation.swift
+++ Concurrency/SyncrounousOperation.swift
@@ -17,7 +17,7 @@ class SyncronousOperation: Operation {
     private var imageURL: URL?
     
     //Public (only get) property to fetch the image set as a result of operation
-    private(set) var image: NSImage?
+    private(set) var image: NSImage? 
     
     //Custom intializer for this class, as this is image load operation, it takes a url to image
     init(with url: URL) {
@@ -26,12 +26,21 @@ class SyncronousOperation: Operation {
     }
     
     //This is the task which operation does. Its the requirement for subclassing Operation.
+    //Its important to check if the operation was cancelled before doing actual work.
+    //Places to check for isCancelled,
+    //Immediately before you perform any actual work
+    //At least once during each iteration of a loop, or more frequently if each iteration is relatively long
+    //At any points in your code where it would be relatively easy to abort the operation
     override func main() {
-        if let imageURL = imageURL,
-            let data = try? Data(contentsOf: imageURL),
-            let image = NSImage(data: data) {
-            self.image = image
+        //check if operation was cancelled before performing any thing
+        if !isCancelled {
+            if let imageURL = imageURL,
+                let data = try? Data(contentsOf: imageURL),
+                let image = NSImage(data: data) {
+                self.image = image
+            }
         }
+        
     }
 
 }
